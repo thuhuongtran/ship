7e441bf41c77e5a0ba339e5e3336eeca19692a62
thuhuongtran <huong.tranthu.hust@gmail.com>
2018-01-16 17:04:53 +0700

    authentication using jwt

---
+++ client/src/main/java/com/vimensa/ship/client/config/security/JWTLoginFilter.java
@@ -0,0 +1,40 @@
+package com.vimensa.ship.client.config.security;
+
+import com.vimensa.ship.client.model.AccountCredentials;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+    public JWTLoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        AccountCredentials credentials = new AccountCredentials(request.getParameter("phone"), request.getParameter("code"));
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        credentials.getPhone(),
+                        credentials.getCode(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        TokenAuthenticationService.addAuthentication(response, authResult.getName());
+    }
+}
---
+++ client/src/main/java/com/vimensa/ship/client/model/AccountCredentials.java
@@ -0,0 +1,30 @@
+package com.vimensa.ship.client.model;
+
+public class AccountCredentials {
+    private String phone;
+    private String code;
+
+    public AccountCredentials() {
+    }
+
+    public AccountCredentials(String phone, String code) {
+        this.phone = phone;
+        this.code = code;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
