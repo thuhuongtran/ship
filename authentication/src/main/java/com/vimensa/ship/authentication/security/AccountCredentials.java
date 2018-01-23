f46297113b64a9f3a66432b11b25eb2dd4f43c9f
thuhuongtran <huong.tranthu.hust@gmail.com>
2018-01-23 14:44:44 +0700

    fixxing authentication from db

--- authentication/src/main/java/com/vimensa/ship/authentication/security/AccountCredentials.java
+++ authentication/src/main/java/com/vimensa/ship/authentication/security/AccountCredentials.java
@@ -7,16 +7,8 @@ public class AccountCredentials {
     public String getPhone() {
         return phone;
     }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getCode() {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
--- authentication/src/main/java/com/vimensa/ship/authentication/security/WebSecurityConfig.java
+++ authentication/src/main/java/com/vimensa/ship/authentication/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vimensa.ship.authentication.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,9 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
@@ -25,12 +30,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(new JWTAuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class);
     }
+    /**
+     * create an account to test
+     * */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         // create a default account
+        /*
         auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("hellohello")
                 .roles("ADMIN");
+        */
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select `phone`,`code`,`enabled` from `client` where `phone`=?")
+                .authoritiesByUsernameQuery("select `phone`,`role` from `user_role` where `phone` = ?");
     }
 }
