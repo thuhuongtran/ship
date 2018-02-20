00de4e2c1e54da17c67f2a8772ab5ce90c940e2c
thuhuongtran <huong.tranthu.vn@gmail.com>
2018-02-21 00:06:50 +0700

    ADMIN: initialize

---
+++ admin/src/main/java/com/vimensa/ship/admin/authentication/security/AccountCredentials.java
@@ -0,0 +1,14 @@
+package com.vimensa.ship.admin.authentication.security;
+
+public class AccountCredentials {
+    private String phone;
+    private String code;
+
+    public String getPhone() {
+        return phone;
+    }
+    public String getCode() {
+        return code;
+    }
+
+}
---
+++ admin/src/main/java/com/vimensa/ship/admin/authentication/security/DataSourceConfig.java
@@ -0,0 +1,19 @@
+package com.vimensa.ship.admin.authentication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://171.244.10.52:3306/ship");
+        driverManagerDataSource.setUsername("misa");
+        driverManagerDataSource.setPassword("VnDgV5*A8m2k16L>?");
+        return driverManagerDataSource;
+    }
+
+}
